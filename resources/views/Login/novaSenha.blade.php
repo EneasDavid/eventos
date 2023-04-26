dd7fbc018fea0615be4b9986a1f9d513e2b1c3c3
David Eneas <david00eneas@gmail.com>
2023-04-26 12:11:06 -0300

    padronização da interface de esqueceu senha

--- resources/views/Login/novaSenha.blade.php
+++ resources/views/Login/novaSenha.blade.php
@@ -2,36 +2,44 @@
 <html lang="pt-br">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Host Event</title>
-    <!--CSS Bootstrap-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <!--CSS -->
-
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+  <title>Host Event</title>
+  <!--CSS-->
+  <link rel="stylesheet" href="/css/style.css">
+  <!--defer faz com que o js seja executado dps q o html for executado-->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"
+        defer></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <!--CSS -->
 </head>
-
-<body>
-      <!--NAVBAR-->
-  <nav class="navbar-dark bg-dark navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand logo" href="/">HostEvent</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav mr-auto mt-2 mt-lg-0" style="float:right">
-        <!--LINKS-->
-        <li class="nav-item">
-          <a class="nav-link" href="/login">logar</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/register">cadastrar</a>
-        </li>
-      </ul>       
-    </div>
-  </nav>
-    <div class="container col-md-6 form-container ">
+    <body>
+        <!--NAVBAR-->
+        <nav class="navbar-dark bg-dark navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand logo" href="/">HostEvent</a>
+            <!--SÓ APARECE NO CELULAR-->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!--SÓ APARECE NO CELULAR-->
+            <!--LINKS-->
+            <div class="collapse navbar-collapse" id="navbarNav" style="justify-content: flex-end;">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0" style="float: right;">
+                        @guest
+                        {{--guest serve pra mostrar coisas pra pessoas não logadas--}}
+                        <li class="nav-item">
+                                <a class="nav-link" onclick="chamaPopUp()" style="cursor:pointer">LOGAR</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" onclick="chamaPopUp()" style="cursor:pointer">CADASTRAR</a>
+                            </li>
+                        @endguest
+            </ul>       
+         </div>
+        </nav>
+    <div class="form-container ">
     @if ($errors->any())
           <div>
             <div class="alert alert-danger">
@@ -49,8 +57,16 @@
           {{ session('danger') }}
         </div>
       @endif 
-        <h1 class="mb-5 m-auto">Nova Senha</h1>
-        <form class="col-md-8 m-auto" action="{{route('recSenhaEntidade')}}" method="POST">
+      </br>
+      </br>
+      </br>
+      </br>
+      </br>
+      </br>
+      </br>
+      <div class="col-md-12 centered my-auto" style="width: max-content;margin-right:10%;margin-left:10%">
+        <h1 class="mb-4 m-auto">Nova Senha</h1>
+        <form class="col-md-10 m-auto" action="{{route('recSenhaEntidade')}}" method="POST">
             @csrf   
             @method('PUT') 
             <input type="hidden" name="entidade" value="{{$entidade->id}}">
@@ -66,7 +82,7 @@
                 </div>
             </div>
         </form>
-
+    </div>
     </div>
 
 
